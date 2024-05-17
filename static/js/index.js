93dbaf4956760e792d3cbfd63d8270a6d0e0c946
name <MY_NAME@example.com>
2024-05-16 23:28:23 -0500

    index

--- static/js/index.js
+++ static/js/index.js
@@ -20,3 +20,27 @@ nextBtn.addEventListener('click', () => {
     }
     carousel.style.transform = `translateX(${-slideWidth * counter}px)`;
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollLeftBtn = document.querySelector('.scroll-left');
+    const scrollRightBtn = document.querySelector('.scroll-right');
+    const reseñasContainer = document.querySelector('.reseñas-container');
+
+    scrollLeftBtn.addEventListener('click', () => {
+        reseñasContainer.scrollBy({
+            top: 0,
+            left: -250,
+            behavior: 'smooth'
+        });
+    });
+
+    scrollRightBtn.addEventListener('click', () => {
+        reseñasContainer.scrollBy({
+            top: 0,
+            left: 250,
+            behavior: 'smooth'
+        });
+    });
+});
+
